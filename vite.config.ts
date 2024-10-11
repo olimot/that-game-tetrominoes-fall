8c655f234666467378a171b813f7297e149fdfc1
seungmkim <seungmkim@gmarket.com>
2024-10-11 17:49:52 +0900

    move source files to src/

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/that-game-tetrominoes-fall/",
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
 });
